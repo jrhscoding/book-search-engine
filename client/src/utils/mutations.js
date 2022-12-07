963abba1222b05ea27c1322da6096a44c290394a
joseph <j.hernandez.92189@gmail.com>
2022-12-06 18:55:39 -0800

    added mutations to src/utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($email: String!, $password: String!) {
+        addUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }   
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String], $title: String!, $bookId: String!, $description: String!, $image: String!, $link: String!) {
+        saveBook(authors: $authors, title: $title, bookId: $bookId, description: $description, image: $image, link: $link) {
+            username
+            savedBooks {
+                _id
+                title
+                bookId
+                description
+                image
+                link
+            }
+        }
+    } 
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                _id
+                title
+                bookId
+                description
+                image
+                link
+            }
+        }    
+    }
+`;
